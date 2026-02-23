0607e79722aba78dc3722c7e92a937335d68be72
LADEESSE <ladeesse.assondji@gaddigital.io>
2026-02-23 13:45:50 +0100

    add Header and Sidebar components

---
+++ src/components/header/header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../styles.css";
+
+function Header() {
+  return (
+    <header className="header">
+        <h1>LOGIN</h1>
+    </header>
+  );
+}
+
+export default Header;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,19 @@
 import ReactDOM from "react-dom/client";
+import Header from "./components/header/header";
+import Siderbar from "./components/siderbar/siderbar";
 
-function Component(props: { a: string }) {
-  return <div className="header">This is right out of the oven: {props.a}</div>;
+function Component() {
+  return (
+    <div className="app">
+      <Header />
+      <div className="content">
+        <Siderbar />
+        <main className="main">
+          <h1>TEAM BOARD</h1>
+        </main>
+      </div>
+    </div>
+  );
 }
 
 const el = document.getElementById("root");

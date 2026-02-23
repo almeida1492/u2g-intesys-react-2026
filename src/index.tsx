1309f7760bb30abe2ee00899d966fa6bea1bd904
hribeiro <henrique.ribeiro@intesys.it>
2026-02-23 11:29:25 +0100

    session 1

---
+++ src/index.tsx
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+
+function Component(props: { a: string }) {
+  return <div className="header">This is right out of the oven: {props.a}</div>;
+}
+
+const el = document.getElementById("root");
+
+if (!el) {
+  throw new Error("Root element not found");
+}
+
+const root = ReactDOM.createRoot(el);
+
+root.render(<Component a="hey, I'm a prop!" />);

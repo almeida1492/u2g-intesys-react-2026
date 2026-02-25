cde96413f98bc7e9520109722a7640eb20b452ab
rivald123 <stephannintedem@gmail.com>
2026-02-25 12:59:12 +0100

    correction des conflits

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,20 @@
-import { useState } from "react";
-import { Header } from "./components/header/Header";
-import { Sidebar } from "./components/sidebar/Sidebar";
-import { Login } from "./pages/Login";
-import { ProjectForm } from "./components/projectForm/ProjectForm";
+import { useState } from 'react'
+import { Header } from './components/header/Header'
+import styles from './globals.module.css'
+import { Dashboard } from './pages/dashboard/Dashboard'
+import { Login } from './pages/login/Login'
 
-export function App(props: { a: string }) {
-  const [isLogged, setIsLogged] = useState(false);
-  const [projects, setProjects] = useState<{ title: string }[]>([]);
-
-  const changeLogState = () => {
-    setIsLogged(true);
-  };
+export function App() {
+  const [appStatus, setAppStatus] = useState<'LOGIN' | 'DASHBOARD'>('LOGIN')
 
   return (
     <div className={styles.app}>
       <Header />
-      <Sidebar />
-
-      {projects.map((project, index) => (
-        <div key={index}>{project.title}</div>
-      ))}
-
-      <ProjectForm
-        handleSubmit={(values) => {
-          setProjects([...projects, { ...values }]);
-        }}
-        // handleDelete={() => {}}
-      />
-
-      {/* {isLogged ? (
-        <p>Dashboard</p>
+      {appStatus === 'DASHBOARD' ? (
+        <Dashboard />
       ) : (
-        <Login setIsLogged={changeLogState}></Login>
-      )} */}
-    </>
-  );
+        <Login goToProjects={() => setAppStatus('DASHBOARD')} />
+      )}
+    </div>
+  )
 }

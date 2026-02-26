3e394d9bae8d27e3b6e14e39229d3b2c71f6a77f
rivald123 <stephannintedem@gmail.com>
2026-02-26 12:36:55 +0100

    resolve conflit

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,10 @@
-import { useState } from 'react'
-import { Header } from './components/header/Header'
-import styles from './globals.module.css'
-import { Dashboard } from './pages/dashboard/Dashboard'
-import { Login } from './pages/login/Login'
+import { BrowserRouter } from 'react-router'
+import { AppRoutes } from './AppRoutes'
 
 export function App() {
-  const [appStatus, setAppStatus] = useState<'LOGIN' | 'DASHBOARD'>('LOGIN')
-
   return (
-    <div className={styles.app}>
-      <Header />
-      {appStatus === 'DASHBOARD' ? (
-        <Dashboard />
-      ) : (
-        <Login goToProjects={() => setAppStatus('DASHBOARD')} />
-      )}
-    </div>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
   )
 }

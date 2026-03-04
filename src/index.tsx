cc541473cf0145520b2c528814ad530c850bbae1
duval2dev <duval_donfack@uni2growcameroun.com>
2026-03-04 12:24:30 +0100

    Fix code while running app

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,12 @@
 import ReactDOM from "react-dom/client";
-
-function Component(props: { a: string }) {
-  return <div className="header">This is right out of the oven: {props.a}</div>;
-}
+import { App } from "./App";
 
 const el = document.getElementById("root");
 
-
 if (!el) {
-    throw new Error("L'élément root n'a pas été trouvé. Vérifie ton fichier index.html");
+  throw new Error("Root element not found");
 }
 
 const root = ReactDOM.createRoot(el);
 
-root.render(<Component a="hey, I'm a prop!" />);
+root.render(<App />);
